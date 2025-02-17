7641b916d9fa55b867bdea4da6f25f23242da680
Timo Frionnet <tfrionnet@gmail.com>
2025-02-17 12:38:23 +0100

    Remove unused PartySocket import

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { useState } from "react";
 import type { Game } from "@/types/game";
-import usePartySocket from "partysocket/react";
 import { createRoom } from "@/app/actions";
 
 export default function Home() {
